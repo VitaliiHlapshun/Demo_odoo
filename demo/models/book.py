79b40da0109f7187f71f1d54b4f40a4830ca04cc
VitaliiHlapshunSmartteksas <vitalii.hlapshun@smartteksas.com>
2021-08-16 00:23:00 +0300

    .gitignore file added and new model book_category

--- demo/models/book.py
+++ demo/models/book.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools.translate import _
 
 
 class LibraryBook(models.Model):
@@ -16,6 +18,11 @@ class LibraryBook(models.Model):
         ('lost', 'Lost')],
         'State', default="draft")
 
+    _sql_constraints = [
+        ('uniqueness_book', 'UNIQUE(name)',
+         'An employee cannot be selected more than once in the mapping. Please remove duplicate(s) and try again.'),
+    ]
+
     @api.model
     def is_allowed_transition(self, old_state, new_state):
         allowed = [('draft', 'available'),
@@ -31,7 +38,8 @@ class LibraryBook(models.Model):
             if book.is_allowed_transition(book.state, new_state):
                 book.state = new_state
             else:
-                continue
+                msg = _(f"You can not change state from '{str.upper(book.state)}' to '{str.upper(new_state)}'!")
+                raise UserError(msg)
 
     def make_available(self):
         self.change_state('available')
@@ -41,3 +49,45 @@ class LibraryBook(models.Model):
 
     def make_lost(self):
         self.change_state('lost')
+
+    def loose_all_book(self):
+        all_books = self.env['library.book']
+        books = all_books.search([])
+        print(f'All books -> {books}')
+        print(f'All books -> {all_books}')
+
+    def change_release(self):
+        # self.ensure_one()
+        self.write({
+            'date_release': fields.Datetime.now(),
+        })
+
+
+    def create_categories(self):
+        categ1 = {
+            'name': 'Child category 1',
+            'description': 'Description for child 1'
+        }
+        categ2 = {
+            'name': 'Child category 2',
+            'description': 'Description for child 2'
+        }
+        parent_category_val = {
+            'name': 'Parent category',
+            'description': 'Description for parent category',
+            'child_ids': [
+                (0, 0, categ1),
+                (0, 0, categ2),
+            ]
+        }
+        # Total 3 records (1 parent and 2 child) will be craeted in library.book.category model
+        record = self.env['library.book.category'].create(parent_category_val)
+        return True
+        # explaining of update() method
+
+    def change_release_date(self):
+        today = fields.Datetime.now()
+        # self.ensure_one()
+        self.update({
+            'date_release': today,
+        })
